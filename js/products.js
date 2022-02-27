818b4a92e2e1709d95d7d3f38bad6068b147787f
Carol Li <dinosaur0514@gmail.com>
2022-02-27 16:22:08 +0800

    資料渲染、展開 modal、新增多圖、新增產品、編輯、刪除

--- js/products.js
+++ js/products.js
@@ -1,10 +1,11 @@
 
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
 
-const url = 'https://vue3-course-api.hexschool.io/v2';
-const path = 'carolli_apexc';
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+const apiPath = 'carolli_apexc';
 
 let productsModal = {}; //建議寫在外面可直接呼叫
+let delProductModal = {};
 
 createApp({
     data(){
@@ -14,12 +15,14 @@ createApp({
                 imagesUrl: [],
             },
             products: [],
+            // 判斷 updateProduct 跑編輯還是新增
+            isNew: false
         }
     },
     methods:{
         // 確認是否有登入
         checkLogin(){
-            axios.post(`${url}/api/user/check`)
+            axios.post(`${apiUrl}/api/user/check`)
             .then((res)=>{
                 // console.log(res);
                 // 成功就執行取得資料
@@ -32,7 +35,7 @@ createApp({
             })
         },
         getData(){
-            axios.get(`${url}/api/${path}/admin/products`)
+            axios.get(`${apiUrl}/api/${apiPath}/admin/products`)
             .then(res=>{
                 console.log(res.data.products);
                 // 成功新增 data 到 products
@@ -46,8 +49,57 @@ createApp({
         showProduct(item){
             this.temp = item;
         },
-        openModal(){
-            productsModal.show();
+        openModal(status, product){
+            console.log(status, product);
+            if( status === 'isNew'){ // 新增產品，temp 等於空的
+                this.temp = {
+                    imagesUrl: [],
+                };
+                productsModal.show();
+                this.isNew = true;
+            }else if(status === 'edit'){ // 編輯產品，temp 就是該產品
+                this.temp = { ...product };
+                productsModal.show();
+                this.isNew = false;
+            }else if(status === 'delete'){ // 刪除產品
+                this.temp = { ...product };
+                delProductModal.show();
+            }
+        },
+        updateProduct(){
+            let url = `${apiUrl}/api/${apiPath}/admin/product`;
+            let method = 'post';
+            if(!this.isNew){ // 如果不是新增產品，把 url 跟 method 做替換
+                url = `${apiUrl}/api/${apiPath}/admin/product/${this.temp.id}`;
+                method = 'put';
+            }
+            // 因為所有資料都要在 data 裡，所以要用 data 帶剛剛新增產品的資料
+            axios[method](url, {data: this.temp})
+            .then(res=>{
+                console.log(res);
+                // 重新取得產品列表
+                this.getData();
+                // 把 modal 關掉
+                productsModal.hide();
+            }).catch(err=>{
+                // 錯誤跳出通知
+                console.dir(err);
+                alert(err.data.message);
+            })
+        },
+        delProduct(){
+            axios.delete(`${apiUrl}/api/${apiPath}/admin/product/${this.temp.id}`)
+            .then(res=>{
+                console.log(res);
+                // 重新取得產品列表
+                this.getData();
+                // 把 modal 關掉
+                delProductModal.hide();
+            }).catch(err=>{
+                // 錯誤跳出通知
+                console.dir(err);
+                alert(err.data.message);
+            })
         }
     },
     mounted(){ //當作 init
@@ -57,5 +109,6 @@ createApp({
         this.checkLogin();
         console.log(token);
         productsModal = new bootstrap.Modal(document.getElementById('productModal'));
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'));
     }
 }).mount('#app');
