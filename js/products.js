1df73c9d5fcb3e0a56c24de5395dc7ef8b7a1def
Carol Li <dinosaur0514@gmail.com>
2022-03-07 17:33:08 +0800

    加入驗證錯誤時，可以跳轉回登入頁面，移除測試用的 console.log，編輯商品加上多圖的陣列

--- js/products.js
+++ js/products.js
@@ -29,20 +29,22 @@ createApp({
                 this.getData();
             })
             .catch((err)=>{
-                console.dir(err);
+                // console.dir(err);
                 // 錯誤跳出通知
-                alert(err.data.message);
+                // alert(err.data.message);
+                // 錯誤返回登入頁面
+                window.location = './login.html';
             })
         },
         getData(){
             axios.get(`${apiUrl}/api/${apiPath}/admin/products`)
             .then(res=>{
-                console.log(res.data.products);
+                // console.log(res.data.products);
                 // 成功新增 data 到 products
                 this.products = res.data.products;
             }).catch(err=>{
                 // 錯誤跳出通知
-                console.dir(err);
+                // console.dir(err);
                 alert(err.data.message);
             })
         },
@@ -50,7 +52,7 @@ createApp({
             this.temp = item;
         },
         openModal(status, product){
-            console.log(status, product);
+            // console.log(status, product);
             if( status === 'isNew'){ // 新增產品，temp 等於空的
                 this.temp = {
                     imagesUrl: [],
@@ -58,7 +60,16 @@ createApp({
                 productsModal.show();
                 this.isNew = true;
             }else if(status === 'edit'){ // 編輯產品，temp 就是該產品
-                this.temp = { ...product };
+                // 如果是「編輯」，就帶入原有資料、把 isNew 變 false。注意：物件傳參考特性，用拷貝才能避免影響到原有的值
+                this.temp = {
+                    ...product
+                };
+                // 當 temp.imagesUrl 有資料時就使用原本的資料，沒有的話就賦予一個陣列
+                this.imagesUrl = this.temp.imagesUrl ? this.temp.imagesUrl : [];
+                // imagesUrl 如果沒有陣列，就補上
+                // if(!this.tempProd.imagesUrl){
+                //     this.tempProd.imagesUrl = [];
+                // }
                 productsModal.show();
                 this.isNew = false;
             }else if(status === 'delete'){ // 刪除產品
@@ -76,28 +87,29 @@ createApp({
             // 因為所有資料都要在 data 裡，所以要用 data 帶剛剛新增產品的資料
             axios[method](url, {data: this.temp})
             .then(res=>{
-                console.log(res);
+                // console.log(res);
                 // 重新取得產品列表
                 this.getData();
                 // 把 modal 關掉
                 productsModal.hide();
             }).catch(err=>{
                 // 錯誤跳出通知
-                console.dir(err);
-                alert(err.data.message);
+                // console.dir(err);
+                // alert(err.data.message);
+                window.location = './login.html';
             })
         },
         delProduct(){
             axios.delete(`${apiUrl}/api/${apiPath}/admin/product/${this.temp.id}`)
             .then(res=>{
-                console.log(res);
+                // console.log(res);
                 // 重新取得產品列表
                 this.getData();
                 // 把 modal 關掉
                 delProductModal.hide();
             }).catch(err=>{
                 // 錯誤跳出通知
-                console.dir(err);
+                // console.dir(err);
                 alert(err.data.message);
             })
         }
